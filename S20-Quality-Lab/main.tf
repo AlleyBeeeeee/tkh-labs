7e7e862d846f43a7610902e0ca6c5fc139721f28
Alexandra Blandon <Alleyb.cruz@gmail.com>
2026-07-28 19:14:51 -0400

    Testing SAST Gate

--- S20-Quality-Lab/main.tf
+++ S20-Quality-Lab/main.tf
@@ -10,17 +10,3 @@ resource "aws_s3_bucket" "vulnerable_vault" {
 resource "random_id" "id" {  
   byte_length = 4  
 }  
-
-# Intentionally vulnerable resource to trigger tfsec failure
-resource "aws_s3_bucket" "vulnerable_bucket" {
-  bucket        = "my-vulnerable-lab-bucket-12345"
-  force_destroy = true
-}
-
-resource "aws_s3_bucket_public_access_block" "vulnerable_bucket_public" {
-  bucket                  = aws_s3_bucket.vulnerable_bucket.id
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
